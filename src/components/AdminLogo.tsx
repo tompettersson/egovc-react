848ad91bcd71ab6372da06f5cc3f3a8c71052527
Thomas Pettersson <thomas.pettersson@actualize.de>
2026-01-15 09:34:06 +0100

    style: Reduce nav icon size from 24px to 18px

--- src/components/AdminLogo.tsx
+++ src/components/AdminLogo.tsx
@@ -44,8 +44,8 @@ export const AdminIcon: React.FC = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="18"
+      height="18"
       viewBox="0 0 52 67"
       fill="currentColor"
     >
